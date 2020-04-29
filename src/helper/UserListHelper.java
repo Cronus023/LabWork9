2737856c9711139dbe5124b7e93285ba543614d7
Cronus023 <anlrey.klochko@mail.ru>
2020-04-29 23:32:57 +0300

    add helper class to save and read ad data from file

--- src/helper/UserListHelper.java
+++ src/helper/UserListHelper.java
@@ -18,8 +18,7 @@ public abstract class UserListHelper {
 	public static UserList readUserList(ServletContext context) {
 		try {
 			USERS_PATH = context.getRealPath(USERS_FILENAME);
-			ObjectInputStream in = new ObjectInputStream(new FileInputStream(
-					USERS_PATH));
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream(USERS_PATH));
 			return (UserList) in.readObject();
 		} catch (Exception e) {
 			return new UserList();
@@ -30,8 +29,7 @@ public abstract class UserListHelper {
 	public static void saveUserList(UserList users) {
 		synchronized (users) {
 			try {
-				ObjectOutputStream out = new ObjectOutputStream(
-						new FileOutputStream(USERS_PATH));
+				ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(USERS_PATH));
 				out.writeObject(users);
 			} catch (Exception e) {
 				e.printStackTrace();
